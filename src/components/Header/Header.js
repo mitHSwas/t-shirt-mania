cf2b7f2459397c67febbeae4ba2c3298b2a9eb44
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-10-08 14:00:45 +0600

    Netlify page not found 404 error solved

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav>
+        <nav className='header'>
             <Link to='/'>Home</Link>
             <Link to='/orders'>Orders</Link>
             <Link to='/grandpa'>Grandpa</Link>
